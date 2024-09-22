c0997415e86d1dc94df1fd053dc6ba310474285c
Kurt Harris <kurtharris1541@gmail.com>
2024-09-22 17:39:53 +0000

    Add CollectionItem model

--- collection/admin.py
+++ collection/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Collections
+from .models import Collections, CollectionItem
 
 admin.site.register(Collections)
+admin.site.register(CollectionItem)
--- collection/models.py
+++ collection/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from profiles.models import Profile
+from brands.models import Series
 
 class Collections(models.Model):
     Profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
@@ -8,6 +9,28 @@ class Collections(models.Model):
 
     def __str__(self):
         return f"{self.Profile.Name}'s collection"
+
+
+class CollectionItem(models.Model):
+    Collection = models.ForeignKey(Collections, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=100)
+    Series = models.ForeignKey(Series, on_delete=models.CASCADE)
+    Quantity = models.PositiveIntegerField(default=1)
+    Image = models.ImageField(
+        upload_to='images/', default='../default_post_e9srbd'
+    )
+
+    constraints = [
+        models.UniqueConstraint(fields=["Collection", "Name", "Series"], name="UX_CollectionItem_Coll_Name_Series")
+    ]
+
+    class Meta:
+        ordering = [
+            "Name","Series"
+        ]
+
+    def __str__(self):
+        return f"{self.Name} - {self.Series} x{self.Quantity}"
 
 
 def create_collection(sender, instance, created, **kwargs):
