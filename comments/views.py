2e85269fca9c5630262110e4b49f6abbdb23aa54
Kurt Harris <kurtharris1541@gmail.com>
2024-10-01 17:17:33 +0000

    Add CommentDetail view

--- comments/views.py
+++ comments/views.py
@@ -2,7 +2,7 @@ from .models import Comment
 from profiles.models import Profile
 from .serializers import CommentSerializer
 from rest_framework import generics, permissions, filters
-from drf_api.permissions import IsOwnerOrReadOnly
+from drf_api.permissions import IsOwnerOrReadOnly_Profile
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -16,3 +16,9 @@ class CommentsList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         logged_in_profile = Profile.objects.filter(User=self.request.user).first()
         serializer.save(Profile = logged_in_profile)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly_Profile]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
