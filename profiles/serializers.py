5cc510b7c0f8800b2f09c0ca00bf48fc7eba3be8
Kurt Harris <kurtharris1541@gmail.com>
2024-10-09 11:58:06 +0000

    Add is_followed field to profiles for easier follow/unfollow functionality

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     OwnerUsername = serializers.ReadOnlyField(source="User.username")
     is_owner = serializers.SerializerMethodField()
+    is_followed = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.User
 
+    def get_is_followed(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            logged_in_profile = Profile.objects.filter(User=user).first()
+            followed = Follower.objects.filter(FollowingProfile=logged_in_profile).filter(FollowedProfile=obj)
+            return True if followed.count()==1 else False
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'User', 'OwnerUsername', 'Created_at', 'ProfileImage', 'Name', "is_owner"    
+            'id', 'User', 'OwnerUsername', 'Created_at', 'ProfileImage', 'Name', "is_owner", "is_followed"
         ]
