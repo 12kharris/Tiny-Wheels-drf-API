4bd3221fe543eb4f36f9eb641c603c2afef8e473
Kurt Harris <kurtharris1541@gmail.com>
2024-10-09 09:49:58 +0000

    Add user id to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
-    Owner = serializers.ReadOnlyField(source="User.username")
+    OwnerUsername = serializers.ReadOnlyField(source="User.username")
     is_owner = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
@@ -12,5 +12,5 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'Owner', 'Created_at', 'Image', 'Name', "is_owner"    
+            'id', 'User', 'OwnerUsername', 'Created_at', 'Image', 'Name', "is_owner"    
         ]
