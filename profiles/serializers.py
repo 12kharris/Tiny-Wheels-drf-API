dd04c519a7d5f7b1434827a16011c6072cd940bc
Kurt Harris <kurtharris1541@gmail.com>
2024-10-10 09:19:02 +0000

    Add collection_id to Profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Profile
 from followers.models import Follower
+from collection.models import Collections
 
 class ProfileSerializer(serializers.ModelSerializer):
     OwnerUsername = serializers.ReadOnlyField(source="User.username")
     is_owner = serializers.SerializerMethodField()
     is_followed = serializers.SerializerMethodField()
+    collection_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -18,8 +20,12 @@ class ProfileSerializer(serializers.ModelSerializer):
             followed = Follower.objects.filter(FollowingProfile=logged_in_profile).filter(FollowedProfile=obj)
             return True if followed.count()==1 else False
 
+    def get_collection_id(self, obj):
+        collection = Collections.objects.filter(Profile=obj).first()
+        return collection.id if collection is not None else ""
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'User', 'OwnerUsername', 'Created_at', 'ProfileImage', 'Name', "is_owner", "is_followed"
+            'id', 'User', 'OwnerUsername', 'Created_at', 'ProfileImage', 'Name', "is_owner", "is_followed", "collection_id"
         ]
