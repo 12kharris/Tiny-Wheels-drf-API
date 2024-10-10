8e536dbf38bad595ffab6658b06f4953eb852c48
Kurt Harris <kurtharris1541@gmail.com>
2024-10-10 13:22:48 +0000

    Add ability to create collection items if logged in

--- collection/urls.py
+++ collection/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('collections/', views.CollecionsList.as_view()),
     path('collections/<int:pk>/', views.CollectionDetail.as_view()),
     path('collections/item/<int:pk>/', views.CollectionItemDetail.as_view()),
+    path('collections/items/', views.CollectionItemList.as_view()),
 ]
--- collection/views.py
+++ collection/views.py
@@ -1,12 +1,13 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseForbidden
 from django.db.models import Count
-from rest_framework import generics, filters
+from rest_framework import generics, filters, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly_CollectionItem
 from .models import Collections, CollectionItem
 from .serializers import CollectionSerializer, CollectionItemSerializer
+from profiles.models import Profile
 
 
 class CollecionsList(generics.ListAPIView):
@@ -38,7 +39,6 @@ class CollectionDetail(APIView):
         if collection is not None:
             #increment the collection's view count
             if (request.user.username != collection.Profile.User.username):
-                print("VIEWS INCREMENTED")
                 collection.Views = collection.Views + 1
                 collection.save()
 
@@ -48,6 +48,20 @@ class CollectionDetail(APIView):
         else: 
             return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
 
+
+class CollectionItemList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = CollectionItemSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.pk is not None:
+            logged_in_profile = Profile.objects.filter(User=user).first()
+            collection = Collections.objects.filter(Profile=logged_in_profile).first()
+            collection_items = CollectionItem.objects.filter(Collection=collection)
+            return collection_items
+        else:
+            return CollectionItem.objects.none()
 
 class CollectionItemDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly_CollectionItem]
